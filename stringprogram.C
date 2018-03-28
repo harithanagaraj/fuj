24f06518f993362c0fab935c8fef85ac5904ed34
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 16:32:29 +0530

    prgm

---
+++ stringprogram.C
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+
+{
+  int size = 4;
+ 
+  char *str = (char *)malloc(sizeof(char)*size);
+  *(str+0) = 'G'; 
+  *(str+1) = 'f';  
+  *(str+2) = 'G';    
+  *(str+3) = '\0';  
+  *(str+1) = 'n'; 
+   getchar();
+   return 0;
+}     
+RString()
+
+char *getString()
+{
+int=getstring()
+  return str;  
+}     
+ 
+int main()
+{
+  printf("%s", getString());  
+  getchar();
+  return 0;return of getString()
+
+char *getString()
+{
+  int size = 4;
+  char *str = (char *)malloc(sizeof(char)*size);
+  *(str+0) = 'G'; 
+  *(str+1) = 'f';  
+  *(str+2) = 'G';
+  *(str+3) = '\0';  
+      
+  return str;  
+}     
+int main()
+{
+  printf("%s", getString());  
+  getchar();
+  return 0;
+}
+ getString() returns.
+
+char *getString()
+{
+  char str[] = "GfG";
+ 
+  return str; 
+}     
+int main()
+{
+  printf("%s", getString());  
+  getchar();
+  return 0;
+}
+
+
+
+
+
+
